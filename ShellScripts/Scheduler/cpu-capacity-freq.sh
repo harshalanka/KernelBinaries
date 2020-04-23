215000bc6185f86081bb85363d05015c8274a3ba
Sriharsha Lanka <slanka@slanka-linux.qualcomm.com>
2020-04-23 13:58:36 +0530

    Removing kernelBinaries

--- ShellScripts/Scheduler/cpu-capacity-freq.sh
+++
@@ -1,9 +0,0 @@
-# enable busybox
-chmod 777 /data/busybox/busybox;
-/data/busybox/busybox --install;
-echo 1 > /sys/module/msm_performance/parameters/core_ctl_register
-killall -9 rt-app
-rm *.log
-rm tidnums.txt
-# start rt-app
-./rt-app taskBoost.json
